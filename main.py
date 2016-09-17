a339889be8c493184e3784e1aecbce40842b2cdc
omkar ukirde <omkar6699@users.noreply.github.com>
2016-09-17 21:48:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from scp import SCPClient
 
 ssh = SSHClient()
 #ssh.load_system_host_keys()
-client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh.connect('example.com')
 
 # SCPCLient takes a paramiko transport as its only argument
