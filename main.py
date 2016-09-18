8751785432cd1794036362523dd07a34b47f786c
omkar ukirde <omkar6699@users.noreply.github.com>
2016-09-18 21:36:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -9,10 +9,11 @@ for byte_3rd in range([5:7]):
   for byte_4th in range([2:255]):
     #ssh.connect('ip','port','user','pass')
     ssh.connect('10.11.{}.{}','22','student','studnet').format(byte_3rd,byte_4th)
-    
+    # SCPCLient takes a paramiko transport as its only argument
+    scp = SCPClient(ssh.get_transport())
     #scp.put('test.txt', 'test2.txt')
     #scp.get('test2.txt')
     scp.close()
 
-# SCPCLient takes a paramiko transport as its only argument
-scp = SCPClient(ssh.get_transport())
+
+
