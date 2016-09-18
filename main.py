b6e4d293eb5f420869e0de9522209251e94a7208
omkar ukirde <omkar6699@users.noreply.github.com>
2016-09-18 21:26:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,12 +5,13 @@ from scp import SCPClient
 ssh = SSHClient()
 #ssh.load_system_host_keys()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-def client(ip,)
-  ssh.connect('example.com')
-  
-  scp.put('test.txt', 'test2.txt')
-  #scp.get('test2.txt')
-  scp.close()
+for byte_3rd in range([5:7]):
+  for byte_4th in range([2:255]):
+    #ssh.connect('ip','port','user','pass')
+    ssh.connect('10.11.{}.{}','22','student','studnet').format(byte_3rd,byte_4th)
+    scp.put('test.txt', 'test2.txt')
+    #scp.get('test2.txt')
+    scp.close()
 
 # SCPCLient takes a paramiko transport as its only argument
 scp = SCPClient(ssh.get_transport())
