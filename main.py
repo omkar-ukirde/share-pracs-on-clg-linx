65fb6fe7dbdfb0a0f55ce37573869df0d8075a4d
omkar ukirde <omkar6699@users.noreply.github.com>
2016-09-21 09:20:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,57 @@
-from subprocess import call
+# lab_spread
 import paramiko
 from scp import SCPClient
-
-ssh = SSHClient()
+import threading
+ssh = paramiko.SSHClient()
 #ssh.load_system_host_keys()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-for byte_3rd in range([5:7]):
-  for byte_4th in range([2:255]):
-    #ssh.connect('ip','port','user','pass')
-    ssh.connect('10.11.{}.{}','22','student','studnet').format(byte_3rd,byte_4th)
-    # SCPCLient takes a paramiko transport as its only argument
-    scp = SCPClient(ssh.get_transport())
-    scp.put('test.txt', 'test2.txt')
-    #scp.get('test2.txt')
-    scp.close()
+def connect_to():
+  for i in range(5,7):
+    for j in range(2,255):
+	    try:
+		    ip='10.11.'+str(i)+'.'+str(j)
+        #ssh.connect('ip','port','user','pass')
+		    print "connecting to "+ip
+		    ssh.connect(ip,22,'root','fedora')
+        # SCPCLient takes a paramiko transport as its only argument
+		    scp = SCPClient(ssh.get_transport())
+		    scp.put('test2.txt',remote_path='/usr/local/bin')
+        #scp.get('test2.txt')
+		    print "file transfered to  "+ip
+		    scp.close()
 
+	    except:
+	      ip='10.11.'+str(i)+'.'+str(j)
+        #ssh.connect('ip','port','user','pass')
+		    print "connecting to "+ip
+		    ssh.connect(ip,22,'student','student')
+        # SCPCLient takes a paramiko transport as its only argument
+		    scp = SCPClient(ssh.get_transport())
+		    scp.put('test2.txt',remote_path='/home/student/')
+        #scp.get('test2.txt')
+		    print "file transfered to  "+ip
+		    scp.close()
+		    print "exception raised for "+ip
+		  
+		  except:
+		     ip='10.11.'+str(i)+'.'+str(j)
+        #ssh.connect('ip','port','user','pass')
+		    print "connecting to "+ip
+		    ssh.connect(ip,22,'Student','student')
+        # SCPCLient takes a paramiko transport as its only argument
+		    scp = SCPClient(ssh.get_transport())
+		    scp.put('test2.txt',remote_path='/home/Student/')
+        #scp.get('test2.txt')
+		    print "file transfered to  "+ip
+		    scp.close()
+		    print "exception raised for "+ip
+		    
+		  else:
+		     print "Tried with Root \n With student \n With Student"
+		     print "exception raised for "+ip
+		    
 
-
+th1 = threading.Thread(target = connection_to)
+th2 = threading.Thread(target = connection_to)
+th1.start()
+th2.start()
